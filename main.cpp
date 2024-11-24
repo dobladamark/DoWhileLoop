97b56bb0222a475f0f941e4a81a1d551dc9f395c
dobladamark <122426238+dobladamark@users.noreply.github.com>
2024-11-24 15:37:13 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   /*int counter = 1;
+   do
+   {
+       cout << "The value is "<<counter<<endl;
+       counter++;
+   }while (counter <= 5);
+   cout << "end";*/
+
+   /*int counter = 1;
+   while (counter <= 1000)
+   {
+       cout << "I love you" <<counter<<endl;
+       counter++;
+   }*/
+
+   for (int counter = 1; counter <= 1 ; counter++)
+   {
+       cout << "*"<<counter;
+    for (int counter1 = 1; counter1 <= 3;  counter--)
+
+    {
+        cout << "*"<<counter1;
+    }
+
+   }
+
+
+}
